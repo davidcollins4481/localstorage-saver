a9605f1737a2c3d55ae956b151ebd8cdf9088406
David Collins <davidcollins4481@gmail.com>
2015-10-04 21:52:22 -0400

    renaming saves

--- popup.js
+++ popup.js
@@ -69,6 +69,45 @@ angular.module('localstorageSaverApp', [])
 
         };
 
+        $scope.promptRename = function() {
+            this.state.renaming = true;
+            console.log(this);
+        };
+
+        $scope.rename = function(state) {
+            if (event.keyCode != 13) {
+                return;
+            }
+
+            this.state.renaming = false;
+            this.state.label = newLabel;
+
+            var newLabel = this.state.edited_label,
+                internalID = this.state.internalID;
+
+            chrome.storage.local.get(null, function (items) {
+                allSites = items['sites'] || {};
+                var currentSite = allSites[$scope.hostname] || {},
+                 keys = Object.keys(currentSite);
+
+                keys.forEach(function(key) {
+                    if (key === internalID) {
+                        currentSite[key].label = newLabel;
+                    }
+                })
+
+                allSites[$scope.hostname] = currentSite;
+                items['sites'] = allSites;
+
+                chrome.storage.local.set(items, function() {
+                    reloadStorageStates();
+                    if (chrome.runtime.error) {
+                        console.log("Runtime error.");
+                    }
+                });
+            });
+        };
+
         $scope.saveStorageState = function() {
             chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
                 chrome.tabs.sendMessage(tabs[0].id, {}, function(response) {
