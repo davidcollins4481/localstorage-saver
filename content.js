583aacd9a599041f9e61dcb4d5783057fa7e3bcb
David Collins <davidcollins4481@gmail.com>
2015-10-04 23:07:27 -0400

    fix typo

--- content.js
+++ content.js
@@ -2,14 +2,14 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.apply) {
             var applySessionStorage  = request.state.sessionStorage,
-                appplyLocalStorage   = request.state.localStorage;
+                applyLocalStorage    = request.state.localStorage;
 
             Object.keys(applySessionStorage).forEach(function(key) {
                 window.sessionStorage[key] = applySessionStorage[key];
             });
 
-            Object.keys(appplyLocalStorage).forEach(function(key) {
-                window.localStorage[key] = appplyLocalStorage[key];
+            Object.keys(applyLocalStorage).forEach(function(key) {
+                window.localStorage[key] = applyLocalStorage[key];
             });
         } else {
             sendResponse({
